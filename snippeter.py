7eeb6fbd3b47323fef93cbd1d0c49332724a4b90
RHSvenson <konto@rhsvenson.dk>
2022-08-29 15:52:19 +0200

    Funktionel iterator færdig. Mangler array.

--- snippeter.py
+++ snippeter.py
@@ -1,5 +1,6 @@
 # Snippeter er programmet ansvarlig for at inddele historien i "snippets" som så kan tilddeles forskellige karakterer
 import re
+import time
 
 
 
@@ -9,31 +10,40 @@ with open("examplestory.txt") as story_file:
     variableCounter = 0
     charCounter = 0
     charCounterTotal = 0
-    snippetArray = {}
+    snippetDictionary = []
 
-    sentenceMatch = re.search("\w|\ ", txt)
+
     # Vi starter med at skippe alt hvad der ikke er alfanumerisk, for at undgå titelformatering mm
-    while re.search("\w| ", story_file[charCounterTotal]):
+    print("Starter scanning...")
+    while re.search("\w| |\"", story_file[charCounterTotal]) == None:
         charCounterTotal += 1
     # Hovedløkke, denne danner en ny streng for hver "adskiller". Typisk et \n eller "
     # Så længe den totale mængde karakterer talt er under den totale mængde i tekst filen, fortsætter vi.
-    while charCounterTotal <= len(story_file):
+    print("Startloop færdig.")
+    print(len(story_file))
+    while charCounterTotal < len(story_file)-1:
             # Derefter starter vi med at lede efter alfanumeriske
-            if story_file[charCounterTotal] == "\D":
+            if re.search("\w|\ |\,|\.|\:|\;|'|\?|\!|-|—|\(|\)", story_file[charCounterTotal]) != None:
                 # Som vi så tilføjer til snippetArray
-                snippetDictionary[variableCounter] += story_file[charCounterTotal]
+                snippetDictionary += story_file[charCounterTotal]
                 charCounterTotal += 1
             # Nye linjer og gåseøjne springes over.
-            elif story_file[charCounterTotal] == "\n":
-                # Lav en intern overspringshandling indtil vi er tilbage til en alfanumerisk, derefter opdater variableCounter.
-                while story_file[charCounterTotal] != "\w":
-                    charCounterTotal +=1
-                variableCounter =+ 1
-            elif story_file[charCounterTotal] == "\"":
+            elif re.search("\n|\r", story_file[charCounterTotal]) != None:
+                # Lav en intern overspringshandling indtil vi er tilbage til en alfanumerisk
+                while re.search("\n|\r", story_file[charCounterTotal]) != None:
+                    charCounterTotal += 1
+                variableCounter += 1
+            elif re.search("\"", story_file[charCounterTotal]) != None:
                 # Spring selve gåseøjnene over, og overgå derefter til en ny variabel.
                 # Bemærk at den tidligere elif for nye linjers while loop betyder at gåseøjne efter \n overspringes
                 # så vi undgår for mange variableCounter switches.
                 charCounterTotal += 1
-                variableCounter += 1
+            else:
+                charCounterTotal += 1
+                print("Fejl, ukendt karakter:", story_file[charCounterTotal])
+
+    print(snippetDictionary)
+    print(charCounterTotal)
+            
 
 
