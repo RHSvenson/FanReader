d3b60cf684392df6b9a0fc44bbd90f0184ffe290
RHSvenson <konto@rhsvenson.dk>
2022-08-31 12:58:34 +0200

    Arbejdede på snippet kode

--- snippeter.py
+++ snippeter.py
@@ -11,7 +11,7 @@ with open("examplestory.txt") as story_file:
     charCounter = 0
     charCounterTotal = 0
     snippetDictionary = []
-
+    stringCache = ""
 
     # Vi starter med at skippe alt hvad der ikke er alfanumerisk, for at undgå titelformatering mm
     print("Starter scanning...")
@@ -24,26 +24,39 @@ with open("examplestory.txt") as story_file:
     while charCounterTotal < len(story_file)-1:
             # Derefter starter vi med at lede efter alfanumeriske
             if re.search("\w|\ |\,|\.|\:|\;|'|\?|\!|-|—|\(|\)", story_file[charCounterTotal]) != None:
+                stringCache += story_file[charCounterTotal]
                 # Som vi så tilføjer til snippetArray
-                snippetDictionary += story_file[charCounterTotal]
                 charCounterTotal += 1
             # Nye linjer og gåseøjne springes over.
-            elif re.search("\n|\r", story_file[charCounterTotal]) != None:
+            elif re.search("\n|\r\\^M", story_file[charCounterTotal]) != None:
                 # Lav en intern overspringshandling indtil vi er tilbage til en alfanumerisk
-                while re.search("\n|\r", story_file[charCounterTotal]) != None:
+                while re.search("\n|\r|\^M", story_file[charCounterTotal]) != None:
                     charCounterTotal += 1
+                # Generer snippets, tildel værdi 0 for højtlæser snippet
+                globals()[f"snippet{variableCounter}"] = (0,str(stringCache))
                 variableCounter += 1
+                snippetDictionary.append([f"snippet{variableCounter}"])
+                stringCache = ""
             elif re.search("\"", story_file[charCounterTotal]) != None:
                 # Spring selve gåseøjnene over, og overgå derefter til en ny variabel.
                 # Bemærk at den tidligere elif for nye linjers while loop betyder at gåseøjne efter \n overspringes
                 # så vi undgår for mange variableCounter switches.
                 charCounterTotal += 1
+                # Tildel værdi 1 for dialog snippet
+                globals()[f"snippet{variableCounter}"] = (1,str(stringCache))
+                variableCounter += 1
+                snippetDictionary.append([f"snippet{variableCounter}"])
+                stringCache = ""
             else:
                 charCounterTotal += 1
                 print("Fejl, ukendt karakter:", story_file[charCounterTotal])
 
     print(snippetDictionary)
     print(charCounterTotal)
+    i = 0
+    while i != 698:
+        print({i}, globals()[f"snippet{i}"])
+        i += 1
             
 
 
