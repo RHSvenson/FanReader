ade26171ca7e82c30e7f240a430906e1552228bf
SejRrr <111116778+SejRrr@users.noreply.github.com>
2022-08-31 13:39:41 +0200

    Director commitment

---
+++ director.py
@@ -0,0 +1,26 @@
+from genericpath import exists
+import re
+from unicodedata import name
+
+
+open with (xamplestory.txt) as director_log:
+
+
+val str = narrator
+    if "i" is within the current_line in narration:
+        then str = "charactertag"
+    elif "i" does not exists within the scope of narration
+        then str = "narratortag"
+
+
+val str = charactertag
+    if line within director_log holds defined name
+        then str = ("charactertag"+(1))
+
+
+srefer to narratortag
+    narrator = 0
+
+
+refer to charactertag
+    characrer = (int+1)
