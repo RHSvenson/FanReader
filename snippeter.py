30d9c0127d4a4a41023a9df8b14a1effcf2a61fd
RHSvenson <konto@rhsvenson.dk>
2022-08-31 13:29:57 +0200

    Tilføjede dokumentation

--- snippeter.py
+++ snippeter.py
@@ -5,37 +5,50 @@ import time
 
 
 with open("examplestory.txt") as story_file:
-    # Tildel en tæller til variablerne så vi dynamisk kan tildele dem i while løkken.
+    # Omdan story_file til en en lang string.
     story_file = story_file.read()
+
+    # Tæller til hvor mange gange vi har skiftet snippet
     variableCounter = 0
-    charCounter = 0
+
+    # Tæller til hvor mange karakterer vi er igennem, til at iterere med
     charCounterTotal = 0
+
+    # Faktisk ikke en dictionary, men en liste. Indeholder vores tuples.
     snippetDictionary = []
+
+    # Midlertidig opbevaring til de sætninger vi bygger.
     stringCache = ""
 
     # Vi starter med at skippe alt hvad der ikke er alfanumerisk, for at undgå titelformatering mm
     print("Starter scanning...")
-    while re.search("\w| |\"", story_file[charCounterTotal]) == None:
+
+    # Så længe vi ikke har nogen alfanumerisk karakter, så springer vi bare over.
+    while re.search("\w|\")", story_file[charCounterTotal]) == None:
         charCounterTotal += 1
     # Hovedløkke, denne danner en ny streng for hver "adskiller". Typisk et \n eller "
     # Så længe den totale mængde karakterer talt er under den totale mængde i tekst filen, fortsætter vi.
     print("Startloop færdig.")
-    print(len(story_file))
     while charCounterTotal < len(story_file)-1:
             # Derefter starter vi med at lede efter alfanumeriske
             if re.search("\w|\ |\,|\.|\:|\;|'|\?|\!|-|—|\(|\)", story_file[charCounterTotal]) != None:
+                # Hver gang vi finder en alfanumerisk, tilføjer vi den til slutningen af vores midlertidige string.
                 stringCache += story_file[charCounterTotal]
-                # Som vi så tilføjer til snippetArray
+
+                # Og øger tælleren med en.
                 charCounterTotal += 1
             # Nye linjer og gåseøjne springes over.
-            elif re.search("\n|\r\\^M", story_file[charCounterTotal]) != None:
-                # Lav en intern overspringshandling indtil vi er tilbage til en alfanumerisk
-                while re.search("\n|\r|\^M", story_file[charCounterTotal]) != None:
+            elif re.search("\n|\r\\^M|\ ", story_file[charCounterTotal]) != None:
+                # Lav en intern overspringshandling indtil vi er tilbage til en alfanumerisk, ligesom i starten.
+                while re.search("\n|\r|\^M|\ ", story_file[charCounterTotal]) != None:
                     charCounterTotal += 1
-                # Generer snippets, tildel værdi 0 for højtlæser snippet
+
+                # Lav dynamiske tuples og tilføj dem til den globale variabel liste
                 globals()[f"snippet{variableCounter}"] = (0,str(stringCache))
-                variableCounter += 1
-                snippetDictionary.append([f"snippet{variableCounter}"])
+                if stringCache != '' or stringCache != ' ':
+                    # Gør den midlertidige string permanent, og tilføj
+                    snippetDictionary.append([f"snippet{variableCounter}"])
+                    variableCounter += 1
                 stringCache = ""
             elif re.search("\"", story_file[charCounterTotal]) != None:
                 # Spring selve gåseøjnene over, og overgå derefter til en ny variabel.
@@ -44,8 +57,9 @@ with open("examplestory.txt") as story_file:
                 charCounterTotal += 1
                 # Tildel værdi 1 for dialog snippet
                 globals()[f"snippet{variableCounter}"] = (1,str(stringCache))
-                variableCounter += 1
-                snippetDictionary.append([f"snippet{variableCounter}"])
+                if stringCache != '' or stringCache != ' ':
+                    snippetDictionary.append([f"snippet{variableCounter}"])
+                    variableCounter += 1
                 stringCache = ""
             else:
                 charCounterTotal += 1
