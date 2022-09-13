0f68a747807ca9105c2c40d6e2a2fe3c03bcc5c4
RHSvenson <konto@rhsvenson.dk>
2022-09-13 09:37:22 +0200

    Fiksede snippeter langt om længe

--- snippeter.py
+++ snippeter.py
@@ -20,58 +20,63 @@ with open("examplestory.txt") as story_file:
     # Midlertidig opbevaring til de sætninger vi bygger.
     stringCache = ""
 
+    # Kontorlelement, håndterer vi en sætning?
+    sentenceOngoing = False
+
     # Vi starter med at skippe alt hvad der ikke er alfanumerisk, for at undgå titelformatering mm
     print("Starter scanning...")
 
     # Så længe vi ikke har nogen alfanumerisk karakter, så springer vi bare over.
-    while re.search("\w|\"", story_file[charCounterTotal]) == None:
-        charCounterTotal += 1
+    #while re.search("\w|\"", story_file[charCounterTotal]) == None:
+    #    charCounterTotal += 1
     # Hovedløkke, denne danner en ny streng for hver "adskiller". Typisk et \n eller "
     # Så længe den totale mængde karakterer talt er under den totale mængde i tekst filen, fortsætter vi.
     print("Startloop færdig.")
     while charCounterTotal < len(story_file)-1:
-            # Derefter starter vi med at lede efter alfanumeriske
-            if re.search("\w|\"|\ |\,|\.|\:|\;|'|\?|\!|-|—|\(", story_file[charCounterTotal]) != None:
-                # Hver gang vi finder en alfanumerisk, tilføjer vi den til slutningen af vores midlertidige string.
-                stringCache += story_file[charCounterTotal]
-
-                # Og øger tælleren med en.
-                charCounterTotal += 1
-            # Nye linjer og gåseøjne springes over.
-            elif re.search("\n|\r\\^M|\.", story_file[charCounterTotal]) != None:
-                # Lav en intern overspringshandling indtil vi er tilbage til en alfanumerisk, ligesom i starten.
-                while re.search("\n|\r|\^M", story_file[charCounterTotal]) != None:
-                    charCounterTotal += 1
-
-                # Lav dynamiske tuples og tilføj dem til den globale variabel liste
-                if re.search("\".+\"", stringCache) != None:
-                    locals()[f"snippet{variableCounter}"] = (1,str(stringCache))
-                    
+            # Kontroller altid først om vi har at gøre med gåseøjne eller andre sætningsindikatorer.
+            if re.search("\"",story_file[charCounterTotal]) != None:
+                # Hvis der allerede er en sætning igang, afslut denne og indlem cache i liste.
+                if sentenceOngoing == True:
+                    globals()[f"snippet{variableCounter}"] = (1,str(stringCache))
+                    sentenceOngoing = False
+                # Ellers så påbegynd en ny sætning, og afslut tidligere narration
                 else:
-                    locals()[f"snippet{variableCounter}"] = (0,str(stringCache))
-                # Gør den midlertidige string permanent, og tilføj
-                snippetDictionary.append([f"snippet{variableCounter}"])
-                variableCounter += 1
+                    globals()[f"snippet{variableCounter}"] = (0,str(stringCache))
+                    sentenceOngoing = True
+                if stringCache != "":
+                    snippetDictionary.append([f"snippet{variableCounter}"])
+                    variableCounter += 1
+
+                # Nulstil og videre
                 stringCache = ""
-            #elif re.search("\"", story_file[charCounterTotal]) != None:
-            #    # Spring selve gåseøjnene over, og overgå derefter til en ny variabel.
-            #    # Bemærk at den tidligere elif for nye linjers while loop betyder at gåseøjne efter \n overspringes
-            #    # så vi undgår for mange variableCounter switches.
-            #    charCounterTotal += 1
-            #    # Tildel værdi 1 for dialog snippet
-            #    globals()[f"snippet{variableCounter}"] = (1,str(stringCache))
-            #    if stringCache != '' or stringCache != ' ':
-            #        snippetDictionary.append([f"snippet{variableCounter}"])
-            #        variableCounter += 1
-            #    stringCache = ""
-            else:
                 charCounterTotal += 1
+                # Bemærk at vi springer gåseøjet over. Vi vil ikke have den med i vores snippet.
+            # Enter og andre paragraf seperatorer springer vi let og elegant over.
+            elif re.search("[\n\r]",story_file[charCounterTotal]):
+                # Indlem først den nuværende string, og så spring over til næste paragraf.
+                if stringCache != "" and stringCache != " ":
+                    snippetDictionary.append([f"snippet{variableCounter}"])
+                    globals()[f"snippet{variableCounter}"] = (0,str(stringCache))
+                    variableCounter += 1
+                stringCache = ""
+                while re.search("[\n\r]", story_file[charCounterTotal]) != None:
+                    charCounterTotal += 1
+            # Hvis der ikke er nogen sætningsindikatorer, så kontrollerer vi efter alfanumeriske.
+            elif re.search("[\w\ \,\.\:\;'\?\!-—\(\)]", story_file[charCounterTotal]):
+                stringCache += story_file[charCounterTotal]
+                charCounterTotal += 1
+            else:
                 print("Fejl, ukendt karakter:", story_file[charCounterTotal])
+                charCounterTotal += 1
+
+
+
+
 
     print(snippetDictionary)
     print(charCounterTotal)
     i = 0
-    while i != 698:
+    while i <= variableCounter:
         print({i}, globals()[f"snippet{i}"])
         i += 1
             
