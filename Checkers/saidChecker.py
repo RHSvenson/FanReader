94d70d50fec5db86e2017d3cc1022822e79c31cf
RHSvenson <konto@rhsvenson.dk>
2022-09-29 09:20:29 +0200

    Omdannede saidChecker og director

--- Checkers/saidChecker.py
+++ Checkers/saidChecker.py
@@ -1,23 +1,27 @@
 # Funktion anvendt til at kontrollere tilstedeværelsen af
 # sætninger som "I said" omkring en snippet
-def saidChecker(snippets, dictionaryPath, charTags):
+def saidChecker(snippet, dictionary, charTags):
     import re
-    with open(dictionaryPath) as file:
-        saidSynonyms = file.read()
-    for analysisObject in snippets:
+    with open(dictionary) as file:
+        saidSynonyms = re.findall(file.read())
+
+    for synonym in saidSynonyms:
         for character in charTags:
-            if re.search(character+" "+saidSynonyms,analysisObject[1]) != None:
-                return character
+            if re.search(character+"\ "+synonym, snippet[1]) != None:
+                phraseCache = (None, snippet[1], ("saidbefore",character))
+                return phraseCache
                 break
-            elif re.search(saidSynonyms+" "+character, analysisObject[1]) != None:
-                return character
+            elif re.search(synonym+"\ "+character, snippet[1]) != None:
+                phraseCache = (None, snippet[1], "saidafter")
+                return phraseCache
                 break
-            elif re.search(saidSynonyms+"\ I", analysisObject[1]) != None:
-                return "Main"
-                break
-            elif re.search("I\ "+saidSynonyms, analysisObject[1]) != None:
-                return "MainX"
+            elif re.search("I\ "+character, snippet[1]) and snippet[0] == 0:
+                phraseCache = (None, snippet[1], ("saidbefore","main"))
+                return phraseCache
                 break
             else:
-                return "F"
+                phraseCache = (None, snippet[1], None)
+                return phraseCache
                 break
+
+    
