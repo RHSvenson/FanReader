edba2a5236aad338f97a90f26bbc70f31ca3212b
RHSvenson <konto@rhsvenson.dk>
2022-09-30 12:53:48 +0200

    begyndte jsonlogger

--- DataTools/jsonLogger.py
+++ DataTools/jsonLogger.py
@@ -1,2 +1,10 @@
 # Denne funktion kaldes når brugeren har sagt ja til at gemme historik.
 # Den skubber alle midlertidige værdier fra director til json filer på den lokale disk.
+
+# Funktionen skal bruge:
+# Historiens navn
+# Primære univers
+# VALGFRIT: Historiens kategorier/tags
+# VALGFRIT: Historiens ukendte navne (typisk genkendte OC's)
+
+def jsonLogger()
