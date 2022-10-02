3dac9b3486249e2f12b54f56e07665280b935763
SejRrr <111116778+SejRrr@users.noreply.github.com>
2022-10-02 12:44:34 +0200

    Main
    bare nogle små ændringer så det er lettere at finde runt i main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Hovedmenu script
 
-#================================================================
+#=============================================================================================
 #Import(s)
-#================================================================
+#=============================================================================================
 
 import imaplib
 from tkinter import *
@@ -15,55 +15,52 @@ from tkinter import filedialog as fd
 import os, glob
 from director import director
 
-#================================================================
+#=============================================================================================
 #Bavgrunds tema
-#================================================================
+#=============================================================================================
 
 #Sætter vores appearance/theme
 customtkinter.set_appearance_mode("Dark")
 
-#================================================================
+#=============================================================================================
 #CTk Root/Main og title
-#================================================================
+#=============================================================================================
 
 #Root/Main (der skal bruges CTk og ikke Tk)
 fr = customtkinter.CTk()
 #Program Title
 fr.title("FanReader")
 
-#================================================================
-#Windows difinacioner
-#================================================================
+#=============================================================================================
+#Windows definitioner
+#=============================================================================================
 
 #window start størrelse
 fr.geometry("860x480")
 #Window min størrelse
 fr.minsize(640,480)
 
-#================================================================
+#=============================================================================================
 #Logo/Icon
-#================================================================
+#=============================================================================================
 
 #Definer PNG som PhotoImage så det kan bruges icon nu og logo senere
 unicon = PhotoImage(file='GarboIconWithBook100px.png')
 #mini icon
 fr.iconphoto(True, unicon)
 
-#================================================================
+#=============================================================================================
 #Grids
-#================================================================
+#=============================================================================================
 
-#En masse Fancy Grids som jeg lige skal læse lidt meget mere op på før det kommer til at se godt ud :/
 fr.grid_columnconfigure(1, weight=1)
 fr.grid_rowconfigure(0, weight=1)
 
+#Definer vænstre side af Menuen
 fr.frame_left = customtkinter.CTkFrame(master=fr,
                                          width=180,
                                          corner_radius=0)
 fr.frame_left.grid(row=0, column=0, sticky="nswe")
-
-fr.frame_right = customtkinter.CTkFrame(master=fr)
-fr.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
 
 #kontrollerer toppen af frame_left
 fr.frame_left.grid_rowconfigure(0, minsize=10)
@@ -72,17 +69,28 @@ fr.frame_left.grid_rowconfigure(8, weight=1)
 fr.frame_left.grid_rowconfigure(9, minsize=20)
 fr.frame_left.grid_rowconfigure(11, minsize=15)
 
-#================================================================
+#Definer højre side af Menuen
+fr.frame_right = customtkinter.CTkFrame(master=fr)
+fr.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
+
+#kontrollerer toppen af frame_right
+fr.frame_right.rowconfigure((0, 1, 2, 3), weight=1)
+fr.frame_right.rowconfigure(7, weight=10)
+#kontrollerer bunden af frame_right
+fr.frame_right.columnconfigure((0, 1), weight=1)
+fr.frame_right.columnconfigure(2, weight=0)
+
+#=============================================================================================
 #Command/def(s)
-#================================================================
+#=============================================================================================
 
 #Command for at tjekke knapperne under debugging og bliver useless i det færdige build
 def click():
     print("Click")
 
-#================================================================
+#=============================================================================================
 #Left Side Logo
-#================================================================
+#=============================================================================================
 
 #FanReader Txet og Logo
 text_var = tkinter.StringVar(value="FanReader")
@@ -96,9 +104,9 @@ fr.label_1 = customtkinter.CTkLabel(master=fr.frame_left,
                                corner_radius=8)
 fr.label_1.grid(row=1, column=0, pady=10, padx=10)
 
-#================================================================
-#Left Side Buttom(s)
-#================================================================
+#=============================================================================================
+#Venstre side knapper(s)
+#=============================================================================================
 
 #Fetcher knap
 fr.button_1 = customtkinter.CTkButton(master=fr.frame_left,
@@ -142,14 +150,9 @@ fr.button_6 = customtkinter.CTkButton(master=fr.frame_left,
                                         command=quit)
 fr.button_6.grid(row=10, column=0, pady=10, padx=20)
 
-#================================================================
+#=============================================================================================
 #Højre side vindu
-#================================================================
-
-fr.frame_right.rowconfigure((0, 1, 2, 3), weight=1)
-fr.frame_right.rowconfigure(7, weight=10)
-fr.frame_right.columnconfigure((0, 1), weight=1)
-fr.frame_right.columnconfigure(2, weight=0)
+#=============================================================================================
 
 fr.frame_info = customtkinter.CTkFrame(master=fr.frame_right)
 fr.frame_info.grid(row=0, column=0, columnspan=2, rowspan=4, pady=20, padx=20, sticky="nsew")
@@ -166,8 +169,8 @@ fr.label_info_1 = customtkinter.CTkLabel(master=fr.frame_info,
                                            justify=tkinter.LEFT)
 fr.label_info_1.grid(column=0, row=0, sticky="nwe", padx=15, pady=15)
 
-#================================================================
+#=============================================================================================
 #Skal slutte med main loop (so don't you dare put anything after this!)
-#================================================================
+#=============================================================================================
 
 fr.mainloop()
