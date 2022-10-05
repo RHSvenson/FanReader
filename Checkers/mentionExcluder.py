5ec14b5671b7004482913c2d59f2a7d9bbd62c99
RHSvenson <konto@rhsvenson.dk>
2022-10-05 09:50:04 +0200

    Startede på mentionexcluder

--- Checkers/mentionExcluder.py
+++ Checkers/mentionExcluder.py
@@ -1,3 +1,12 @@
 # Denne checker udelukker en karakter nævnt i en sætning fra at eje sætningen.
+# Bemærk at denne kun skal kaldes hvis karakteren har normal udtryk.
+# Altså skal den udelukkes for karakterer der taler i f.eks. tredjeperson.
 
 def mentionExcluder(snippet, charTags):
+    import re
+
+    for character in charTags:
+        if re.search(character, snippet) != None:
+            return True
+        else:
+            return False
